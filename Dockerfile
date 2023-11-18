b285487d9d0a2b113a803f24e17c9acb6f944f4a
Steven Frew <steven.fullhouse@gmail.com>
2023-11-18 09:36:36 +0000

    .net8, and more configurable endpoints

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,16 @@
 ARG version="1.0.0.0"
+ARG dotNetVersion="8.0"
 
-FROM mcr.microsoft.com/dotnet/sdk AS build
+FROM mcr.microsoft.com/dotnet/sdk:${dotNetVersion} AS build
 ARG version
 WORKDIR /GitLabSymbolServerProxy
 COPY ./GitLabSymbolServerProxy /GitLabSymbolServerProxy
 RUN dotnet publish -c Release -r linux-musl-x64 --self-contained /p:Version=${version}
 
 FROM alpine:latest
+ARG dotNetVersion
 WORKDIR /GitLabSymbolServerProxy
-COPY --from=build /GitLabSymbolServerProxy/bin/Release/net6.0/linux-musl-x64/publish/* ./
+COPY --from=build /GitLabSymbolServerProxy/bin/Release/net${dotNetVersion}/linux-musl-x64/publish/* /GitLabSymbolServerProxy/appsettings.yml ./
 COPY --from=build /GitLabSymbolServerProxy/certs/* ./certs/
-COPY --from=build /GitLabSymbolServerProxy/appsettings.yml ./
-RUN apk add gcompat
-RUN apk add libstdc++
-RUN apk add icu-libs
+RUN apk add gcompat libstdc++ icu-libs
 ENTRYPOINT ["/GitLabSymbolServerProxy/GitLabSymbolServerProxy"]
--- GitLabSymbolServerProxy/Program.cs
+++ GitLabSymbolServerProxy/Program.cs
@@ -46,9 +46,21 @@ public static class KestrelServerOptionsExtensions {
 				return endpoint;
 			});
 
-		foreach (var endpoint in endpoints.Values) {
+		static int getPort(string? port, string scheme) {
+			var schemePort = scheme == EndpointConfiguration.HttpsScheme ? 443 : 80;
+			if (int.TryParse(port, out var parsedPort))
+				return parsedPort;
+			if (!string.IsNullOrEmpty(port)) {
+				var envPort = Environment.GetEnvironmentVariable(port);
+				if (int.TryParse(envPort, out parsedPort))
+					return parsedPort;
+			}
+			return schemePort;
+		}
+
+		foreach (var endpoint in endpoints.Values.Where(endpoint => endpoint.Enabled)) {
 			var config = endpoint;
-			var port = config.Port ?? (config.Scheme == EndpointConfiguration.HttpsScheme ? 443 : 80);
+			var port = getPort(config.Port, config.Scheme);
 			static IReadOnlyCollection<IPAddress> getIPAddresses(string host) {
 				if (host == EndpointConfiguration.LocalHost)
 					return new[] { IPAddress.IPv6Loopback, IPAddress.Loopback };
@@ -59,12 +71,13 @@ public static class KestrelServerOptionsExtensions {
 
 			foreach (var address in getIPAddresses(config.Host))
 				options.Listen(address, port,
-						listenOptions => {
-							if (config.Scheme == "https") {
-								var certificate = LoadCertificate(config, environment);
-								listenOptions.UseHttps(certificate);
-							}
-						});
+					listenOptions => {
+						if (config.Scheme == "https") {
+							var certificate = LoadCertificate(config, environment);
+							listenOptions.UseHttps(certificate);
+						}
+					}
+				);
 		}
 	}
 
@@ -90,12 +103,13 @@ public static class KestrelServerOptionsExtensions {
 }
 
 public class EndpointConfiguration {
-	public const string LocalHost = "localhost";
+	public const string LocalHost = "0.0.0.0";
 	public const string HttpScheme = "http";
 	public const string HttpsScheme = "https";
 
 	public string Host { get; set; } = LocalHost;
-	public int? Port { get; set; }
+	public bool Enabled { get; set; } = true;
+	public string? Port { get; set; }
 	public string Scheme { get; set; } = HttpScheme;
 	public string? StoreName { get; set; }
 	public string? StoreLocation { get; set; }
